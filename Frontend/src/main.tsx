bf1797f16c868b34b0462679bd0ea409d825e171
LupusWhiteFox <observative2oppp@gmail.com>
2025-07-24 17:12:47 +0700

    den

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.tsx'
 import LandingPage from './page/LandingPage.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LandingPage />
+    <App />
   </StrictMode>,
 )
