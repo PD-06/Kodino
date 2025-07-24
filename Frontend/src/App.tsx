991020f8dff10417a3d1b582dcb83ea67322ee51
DeffreusTheda <vinnie@Deffreuss-MacBook-Air.local>
2025-07-24 19:58:55 +0700

    push push
    Signed-off-by: DeffreusTheda <vinnie@Deffreuss-MacBook-Air.local>

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './page/LandingPage';
 import TentangPage from './page/TentangPage';
 import MateriPage from './page/MateriPage';
+import Pendahuluan from './page/materi/Pendahuluan';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/tentang" element={<TentangPage />} />
         <Route path="/materi" element={<MateriPage />} />
+        <Route path="/materi/pendahuluan" element={<Pendahuluan />} />
       </Routes>
     </Router>
   );
